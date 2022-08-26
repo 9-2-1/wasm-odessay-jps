2f2462bc38ae833036a3a77cac359fb88dd4f659
six-6 <1195132772@qq.com>
2022-08-26 08:33:34 +0800

    page演示

---
+++ pkg/wasm_odessay_jps.js
@@ -0,0 +1,152 @@
+let wasm_bindgen;
+(function() {
+    const __exports = {};
+    let wasm;
+
+    let cachedUint8Memory0 = new Uint8Array();
+
+    function getUint8Memory0() {
+        if (cachedUint8Memory0.byteLength === 0) {
+            cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+        }
+        return cachedUint8Memory0;
+    }
+
+    let WASM_VECTOR_LEN = 0;
+
+    function passArray8ToWasm0(arg, malloc) {
+        const ptr = malloc(arg.length * 1);
+        getUint8Memory0().set(arg, ptr / 1);
+        WASM_VECTOR_LEN = arg.length;
+        return ptr;
+    }
+
+    let cachedInt32Memory0 = new Int32Array();
+
+    function getInt32Memory0() {
+        if (cachedInt32Memory0.byteLength === 0) {
+            cachedInt32Memory0 = new Int32Array(wasm.memory.buffer);
+        }
+        return cachedInt32Memory0;
+    }
+
+    function getArrayI32FromWasm0(ptr, len) {
+        return getInt32Memory0().subarray(ptr / 4, ptr / 4 + len);
+    }
+    /**
+    * @param {Uint8Array} map
+    * @param {number} map_x
+    * @param {number} map_y
+    * @param {number} begin_x
+    * @param {number} begin_y
+    * @param {number} end_x
+    * @param {number} end_y
+    * @returns {Int32Array}
+    */
+    __exports.a_star_jps = function(map, map_x, map_y, begin_x, begin_y, end_x, end_y) {
+        try {
+            const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+            var ptr0 = passArray8ToWasm0(map, wasm.__wbindgen_malloc);
+            var len0 = WASM_VECTOR_LEN;
+            wasm.a_star_jps(retptr, ptr0, len0, map_x, map_y, begin_x, begin_y, end_x, end_y);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            var v1 = getArrayI32FromWasm0(r0, r1).slice();
+            wasm.__wbindgen_free(r0, r1 * 4);
+            return v1;
+        } finally {
+            wasm.__wbindgen_add_to_stack_pointer(16);
+            map.set(getUint8Memory0().subarray(ptr0 / 1, ptr0 / 1 + len0));
+            wasm.__wbindgen_free(ptr0, len0 * 1);
+        }
+    };
+
+    async function load(module, imports) {
+        if (typeof Response === 'function' && module instanceof Response) {
+            if (typeof WebAssembly.instantiateStreaming === 'function') {
+                try {
+                    return await WebAssembly.instantiateStreaming(module, imports);
+
+                } catch (e) {
+                    if (module.headers.get('Content-Type') != 'application/wasm') {
+                        console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                    } else {
+                        throw e;
+                    }
+                }
+            }
+
+            const bytes = await module.arrayBuffer();
+            return await WebAssembly.instantiate(bytes, imports);
+
+        } else {
+            const instance = await WebAssembly.instantiate(module, imports);
+
+            if (instance instanceof WebAssembly.Instance) {
+                return { instance, module };
+
+            } else {
+                return instance;
+            }
+        }
+    }
+
+    function getImports() {
+        const imports = {};
+        imports.wbg = {};
+
+        return imports;
+    }
+
+    function initMemory(imports, maybe_memory) {
+
+    }
+
+    function finalizeInit(instance, module) {
+        wasm = instance.exports;
+        init.__wbindgen_wasm_module = module;
+        cachedInt32Memory0 = new Int32Array();
+        cachedUint8Memory0 = new Uint8Array();
+
+
+        return wasm;
+    }
+
+    function initSync(bytes) {
+        const imports = getImports();
+
+        initMemory(imports);
+
+        const module = new WebAssembly.Module(bytes);
+        const instance = new WebAssembly.Instance(module, imports);
+
+        return finalizeInit(instance, module);
+    }
+
+    async function init(input) {
+        if (typeof input === 'undefined') {
+            let src;
+            if (typeof document === 'undefined') {
+                src = location.href;
+            } else {
+                src = document.currentScript.src;
+            }
+            input = src.replace(/\.js$/, '_bg.wasm');
+        }
+        const imports = getImports();
+
+        if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+            input = fetch(input);
+        }
+
+        initMemory(imports);
+
+        const { instance, module } = await load(await input, imports);
+
+        return finalizeInit(instance, module);
+    }
+
+    wasm_bindgen = Object.assign(init, __exports);
+
+})();
